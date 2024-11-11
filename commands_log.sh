8b8539fbf56f65297b0b9bafec476e576034bf51
G.W. <Greg@localhost>
2024-11-11 13:11:29 +0100

    += commands_log.sh

--- commands_log.sh
+++ commands_log.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 
+#./phind/gen_markdown_report_question_build.sh | tee phind/phind-Q03.md | xcc ; vim phind/phind-Q03.md
 aider --4o build.sh Dockerfile reveal.json  README.md -f phind/phind-Q03.md 
