7c002db0fd1f924538fc7bf7634c933f5d26b3c5
G.W. <Greg@localhost>
2024-11-11 14:02:11 +0100

    += commands_log.sh

--- commands_log.sh
+++ commands_log.sh
@@ -4,14 +4,14 @@ set -x
 function xcc() { xclip -sel clipboard; }
 
 function gen_build_sh_md_and_aider_v01() {
+local QFILE="$1"
+shift 1
 for f in "$@" ; do
   if [ ! -r "$f" ]; then
     echo "ERROR:FILE_NOT_READABLE:$f"
     exit 1
   fi
 done
-local QFILE="$1"
-shift 1
 if [ -r "$QFILE" ]; then echo "ERROR: FILE $QFILE ALREADY EXISTS!"; exit 1; fi
 ./phind/gen_markdown_report_question_build.sh | tee "$QFILE" | xcc && vim "$QFILE"|| exit 1
 read -p 'Shall we proceed with aider? [yes/y]' answer
