61dac478fa1c20f8d58bc4117071d2f48c1e39cc
G.W. <Greg@localhost>
2024-11-11 13:20:52 +0100

    += commands_log.sh

--- commands_log.sh
+++ commands_log.sh
@@ -5,7 +5,12 @@ function gen_build_sh_md_and_aider_v00() {
 local QFILE="$1"
 if [ -r "$QFILE" ]; then echo "ERROR: FILE $QFILE ALREADY EXISTS!"; exit 1; fi
 ./phind/gen_markdown_report_question_build.sh | tee "$QFILE" | xcc ; vim "$QFILE"
-aider --4o build.sh Dockerfile reveal.json  README.md -f "$QFILE" 
+read -p 'Shall we proceed with aider? [yes/y]' answer
+if [ "$answer" != "yes" ] && [ "$answer" != "y" ]; then
+  exit 1
+else
+  aider --4o build.sh Dockerfile reveal.json  README.md -f "$QFILE" 
+fi
 }
 
 # gen_build_sh_md_and_aider_v00 phind/phind-Q03.md
