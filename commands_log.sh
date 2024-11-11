cad026da1575f73801327c311fd527f1771394fe
G.W. <Greg@localhost>
2024-11-11 13:30:16 +0100

    += commands_log.sh

--- commands_log.sh
+++ commands_log.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
+alias xcc='xclip -sel clipboard'
 
 function gen_build_sh_md_and_aider_v00() {
 local QFILE="$1"
 if [ -r "$QFILE" ]; then echo "ERROR: FILE $QFILE ALREADY EXISTS!"; exit 1; fi
-./phind/gen_markdown_report_question_build.sh | tee "$QFILE" | xcc ; vim "$QFILE"
+./phind/gen_markdown_report_question_build.sh | tee "$QFILE" | xcc && vim "$QFILE"|| exit 1
 read -p 'Shall we proceed with aider? [yes/y]' answer
 if [ "$answer" != "yes" ] && [ "$answer" != "y" ]; then
   exit 1
