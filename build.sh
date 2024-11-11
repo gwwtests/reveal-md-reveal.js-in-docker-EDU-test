0a763368a9392bf1439369edbf7641ab605b3c49
G.W. <Greg@localhost>
2024-11-11 13:06:35 +0100

    += phind/gen_markdown_report_question_build.sh build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo 'set -x'
 set -x
 
-# Build Docker image
-docker build -t reveal-static .
+# Build Docker image (show last 25 lines)
+docker build --progress=plain -t reveal-static . 2>&1 | tail -n 25
 
 # Run container to generate static content
 docker run --rm -v $(pwd):/presentation reveal-static
--- phind/gen_markdown_report_question_build.sh
+++ phind/gen_markdown_report_question_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo '# Question
+echo '
+Please resolve issue. Here I provide context I provided other asisstant and notes they prepared, you may find them useful.
+
+# Question
 
 Script: `$ cat build.sh` :
 
@@ -8,11 +11,11 @@ Script: `$ cat build.sh` :
 cat build.sh
 echo '```
 
-Output: `bash build.sh` :
+Output: `bash build.sh 2>&1` :
 
 ```'
 
-bash build.sh
+bash build.sh 2>&1
 
 echo '```
 
