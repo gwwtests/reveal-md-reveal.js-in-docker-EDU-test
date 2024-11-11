33df185d002ba2459781a4b63bcdd8e9b6737f23
G.W. <Greg@localhost>
2024-11-11 13:59:11 +0100

    += commands_log.sh

--- commands_log.sh
+++ commands_log.sh
@@ -3,18 +3,33 @@ set -x
 
 function xcc() { xclip -sel clipboard; }
 
-function gen_build_sh_md_and_aider_v00() {
+function gen_build_sh_md_and_aider_v01() {
+for f in "$@"
+do
+  if [ ! -r "$f" ]
+  then
+    echo "ERROR:FILE_NOT_READABLE:$f"
+    exit 1
+  fi
+done
 local QFILE="$1"
+shift 1
 if [ -r "$QFILE" ]; then echo "ERROR: FILE $QFILE ALREADY EXISTS!"; exit 1; fi
 ./phind/gen_markdown_report_question_build.sh | tee "$QFILE" | xcc && vim "$QFILE"|| exit 1
 read -p 'Shall we proceed with aider? [yes/y]' answer
 if [ "$answer" != "yes" ] && [ "$answer" != "y" ]; then
   exit 1
 else
-  aider --4o build.sh Dockerfile reveal.json  README.md -f "$QFILE" 
+  aider --4o build.sh -f "$QFILE" "$@"
 fi
+}
+
+function gen_build_sh_md_and_aider_v00() {
+gen_build_sh_md_and_aider_v01 "$QFILE" Dockerfile reveal.json  README.md 
 }
 
 # gen_build_sh_md_and_aider_v00 phind/phind-Q03.md
 # gen_build_sh_md_and_aider_v00 phind/phind-Q04.md
 # gen_build_sh_md_and_aider_v00 phind/phind-Q05.md
+# gen_build_sh_md_and_aider_v00 phind/phind-Q06.md
+gen_build_sh_md_and_aider_v00 phind/phind-Q07.md Dockerfile reveal.json README.md slides/slides.md template.html
